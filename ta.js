f691112bb23746811f8e9f38abae4791e09b7eae
Taha Khan <102149347+taha4khan@users.noreply.github.com>
2022-04-07 20:15:04 +0500

    6 april lab

---
+++ ta.js
@@ -0,0 +1,48 @@
+userdata = [
+        {
+            name:"taha",
+            pass: "123"
+
+        },
+        {
+            name: "khubaib",
+            pass:"1234"
+        },
+        {
+            name: "hassan",
+            pass:"12345"
+        }       
+             
+]
+function adduser(adnan,123456) {
+    newdata={
+        name: user4
+        pass:
+    }
+    userdata.push();
+
+           }
+
+    
+    
+    
+}
+
+
+function getuser(){
+    userdata.forEach(userdata => {
+        console.log(user)
+    });
+}
+
+function login(user1,pass1){
+    if  (userdata==user1){
+        console.log("userdata login")
+    }else{
+        console.log("userdata not found")
+    }
+    
+
+    
+}
+userdata(taha,123)
